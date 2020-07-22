8fed168f8d897c2dfa653db3353e11dfe44e2fbc
Kollc <46793445+Kollc@users.noreply.github.com>
2020-07-22 23:18:25 +0300

    доработал модуль отрисовки похожишь волшебников и устранил дребезг

--- js/createPers.js
+++ js/createPers.js
@@ -1,33 +1,31 @@
 'use strict';
 
 (function () {
-
-  var setup = document.querySelector('.setup');
-  var template = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-  var similarListElement = setup.querySelector('.setup-similar-list');
-  var fragment = document.createDocumentFragment();
   var MAX_SIMILAR_WIZARD_COUNT = 4;
+  var wizardTemplate = document.querySelector('#similar-wizard-template');
 
-  // создание элемента
   var renderWizard = function (wizard) {
-    var wizardElement = template.cloneNode(true);
+    var element = wizardTemplate.content.cloneNode(true);
 
-    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    var wizardElement = element.querySelector('.wizard');
     wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
     wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
-    return wizardElement;
+    element.querySelector('.setup-similar-label').innerText = wizard.name;
+
+    return element;
   };
 
-  // загружаем магов
-  window.load(function (wizards) {
-    var wizardsCurrent = window.randomGenerateSomeElement(wizards);
+  var similar = document.querySelector('.setup-similar');
+  var similarList = document.querySelector('.setup-similar-list');
 
-    for (var i = 0; i < MAX_SIMILAR_WIZARD_COUNT; i++) {
-      fragment.appendChild(renderWizard(wizardsCurrent[i]));
+  window.render = function (data) {
+    var takeNumber = data.length > MAX_SIMILAR_WIZARD_COUNT ? MAX_SIMILAR_WIZARD_COUNT : data.length;
+    similarList.innerHTML = '';
+    for (var i = 0; i < takeNumber; i++) {
+      similarList.appendChild(renderWizard(data[i]));
     }
-    similarListElement.appendChild(fragment);
 
-    setup.querySelector('.setup-similar').classList.remove('hidden');
-  }, window.getError);
+    similar.classList.remove('hidden');
+  };
 })();
--- js/settingPers.js
+++ js/settingPers.js
@@ -2,25 +2,45 @@
 
 (function () {
   var form = document.querySelector('.setup-wizard-form');
+  var fireballTrigger = document.querySelector('.setup-fireball-wrap');
   var coutColorTrigger = document.querySelector('.setup-wizard .wizard-coat');
   var eyesColorTrigger = document.querySelector('.setup-wizard .wizard-eyes');
-  var fireballTrigger = document.querySelector('.setup-fireball-wrap');
   var setup = document.querySelector('.setup');
 
   setup.querySelector('.setup-similar').classList.remove('hidden');
 
   document.querySelector('.setup-similar').classList.remove('hidden');
 
+  // дефолтные характеристики персонажа
+  var myWizard = {
+    coatColor: 'rgb(101, 137, 164)',
+    eyesColor: 'black'
+  };
+
+  var wizards = [];
+
   // дейсвтие смены цвета мантии
   coutColorTrigger.addEventListener('click', function () {
-    coutColorTrigger.style.fill = window.randomGenerate(window.data.coatColors);
-    document.querySelector('input[name="coat-color"]').value = window.randomGenerate(window.data.coatColors); // изменяем значения input для отправки на сервер
+    var color = window.randomGenerate(window.data.coatColors);
+    coutColorTrigger.style.fill = color;
+    document.querySelector('input[name="coat-color"]').value = color; // изменяем значения input для отправки на сервер
+
+    myWizard.coatColor = color;
+    window.debounce(function () {
+      updateWizards();
+    });
   });
 
   // дейсвтие смены цвета глаз
   eyesColorTrigger.addEventListener('click', function () {
-    eyesColorTrigger.style.fill = window.randomGenerate(window.data.eyesColors);
-    document.querySelector('input[name="eyes-color"]').value = window.randomGenerate(window.data.eyesColors); // изменяем значения input для отправки на сервер
+    var color = window.randomGenerate(window.data.eyesColors);
+    eyesColorTrigger.style.fill = color;
+    document.querySelector('input[name="eyes-color"]').value = color; // изменяем значения input для отправки на сервер
+
+    myWizard.eyesColor = color;
+    window.debounce(function () {
+      updateWizards();
+    });
   });
 
   // дейсвтие смены цвета фаербола
@@ -37,6 +57,40 @@
       setup.classList.add('hidden'); // закрываем popup
     }, window.getError);
   };
+
+  // функция расчета баллов
+  var getRank = function (wizard) {
+    var rank = 0;
+
+    if (wizard.colorCoat === myWizard.coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === myWizard.eyesColor) {
+      rank += 1;
+    }
+
+    return rank;
+  }
+
+  // сортируем и обновляем магов на похожих
+  var updateWizards = function () {
+    window.render(wizards.slice().sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = wizards.indexOf(left) - wizards.indexOf(right);
+      }
+
+      return rankDiff;
+    }));
+  }
+
+  // обновляем магов на похожишь, сразу после прихода данных с сервера
+  var successHandler = function (data) {
+    wizards = data;
+    updateWizards();
+  };
+
+  window.load(successHandler, getError);
 
   form.addEventListener('submit', submitHandler);
 
